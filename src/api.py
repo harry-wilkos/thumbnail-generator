62ac8ec827a94641555e97c447d70c060337d6d9
Harry Wilkinson <harrywi@ws26.london.etc>
2024-09-04 17:21:06 +0100

    comatible with ETC Farn

---
+++ src/api.py
@@ -0,0 +1,214 @@
+import os
+from frame_analysis import main, get_quality
+from concurrent.futures import ProcessPoolExecutor
+from datetime import datetime
+import inspect
+from pathlib import PurePath
+import glob
+import json
+from bson.objectid import ObjectId
+try:
+    from fastapi import FastAPI, Request
+    import uvicorn
+    from pymongo import MongoClient
+    import clique
+    import requests
+    from dotenv import load_dotenv
+except ImportError:
+    import pip, site
+    from importlib import reload
+    pip.main(["install", "fastapi", "-q"])
+    pip.main(["install", "uvicorn", "-q"])
+    pip.main(["install", "requests", "-q"])
+    pip.main(["install", "clique", "-q"])
+    pip.main(["install", "python-dotenv", "-q"])
+    reload(site)
+    from fastapi import FastAPI, Request
+    from pymongo import MongoClient
+    import uvicorn
+    import clique
+    import requests
+    from dotenv import load_dotenv
+load_dotenv()
+
+
+def index(input, address):
+
+    # Get Mongo Database
+    client = MongoClient(address[0])
+    db_name = address[1]
+    collection_name = address[2]
+    if db_name in client.list_database_names():
+        db = client.get_database(db_name)
+    else:
+        db = client[db_name]
+    if collection_name in db.list_collection_names():
+        collection = db.get_collection(collection_name)
+    else:
+        collection = db[collection_name]
+
+    # Check for existing doc
+    doc = collection.find_one({"path":input})
+    if doc is None:
+        return [collection]
+    else:
+        return collection, doc["_id"], doc["quality"], doc["thumbnail"], doc["num_frames"], doc["processing"]
+
+def request(CUE_API_URL, API_VERSION, method, data=None):
+    url = f"http://{CUE_API_URL}/api/{API_VERSION}/{method}"
+    if not data:
+        resp = requests.get(url)
+    else:
+        headers = {"Content-Type": "application/json"}
+        resp = requests.post(url, headers=headers, json=data)
+    return resp.json()
+
+
+def process (input, frame_cap, quality, color_weight, focus_weight, farm, threads, db_address):
+    
+    tag = input
+    path = PurePath(input)
+    if len(path.suffixes) != 1:
+        pattern = path.name.split(".")[0] + "*" + path.suffix 
+        files = glob.glob(str(PurePath(path.parent / pattern)))
+        assembly = clique.assemble(files)[0][0]
+        tag = assembly.head + str(assembly.indexes) + assembly.tail
+
+    id_dic = index(tag, db_address)
+    collection = id_dic[0]
+
+    id = None
+    num_frames = None
+    store_quality = [frame_cap, quality, None]
+    if len(id_dic) != 1:
+        if id_dic[5] is True:
+            return id_dic[3], id
+        store_quality = get_quality(frame_cap, quality, id_dic[4])
+        if store_quality[1] <= id_dic[2]:
+            return id_dic[3], id
+        else:
+            id = str(id_dic[1])
+
+    if id is None:
+        doc = collection.insert_one({
+            "thumbnail": None,
+            "quality": store_quality[1],
+            "path": tag,
+            "num_frames": num_frames,
+            "processing": True
+        })
+        id = str(doc.inserted_id)
+    else:
+        collection.update_one({"_id": ObjectId(id)},{
+            "$set":{
+                "quality": store_quality[1],
+                "processing": True
+            }
+        })
+    
+    if farm is False:
+        result = main(input, store_quality, color_weight, focus_weight, db_address, id)
+
+    else:           
+        args = {
+            "input": input, 
+            "store_quality": store_quality, 
+            "color_weight": color_weight, 
+            "focus_weight": focus_weight,
+            "db_address": db_address,
+            "id": id
+        }
+        
+        package_path = os.environ.get("RUN_FRAME_ANALYSIS", os.path.abspath(inspect.getmodule(main).__file__))
+
+        layers ={
+                "type": "base",
+                "service": "postprocess",
+                "kwargs": {
+                    "name": f"target_{path.__hash__()}",
+                    "command": f"rez-env ffmpeg -- {os.environ["RUN_FRAME_ANALYSIS"]} '{json.dumps(args)}'"
+                },
+                "settings": {
+                    "start": 1001,
+                    "end": 1001,
+                    "step": 1,
+                    "chunk_size": 1,
+                    "cores": threads
+                }
+            }
+        return layers, id
+
+    return result, id
+        
+
+def parallel (input, frame_cap = 100, quality = 0.5, color_weight = -1, focus_weight = -1, farm = True, priority = 100, farm_threads = 4, 
+             db_address = [os.environ["MONGO_URL"],"frame_analysis","thumbnail_gen"], cue_address = [os.environ["CUE_API_URL"], os.environ["CUE_version"],], submission_threads = None):
+    
+    outline = {
+        "name": f"thumnail_gen_{datetime.now().strftime('%H-%M-%S_%d-%m-%y')}",
+        "priority": priority,
+    }
+    single = False
+    if type(input) is str:
+        results_tuple = [process(input, frame_cap, quality, color_weight, focus_weight, farm, farm_threads, db_address)]
+        results = [results_tuple[0][0]]
+        single = True
+    else:
+        with ProcessPoolExecutor(submission_threads) as exe:
+            futures = [exe.submit(process, i, frame_cap, quality, color_weight, focus_weight, farm, farm_threads, db_address) for i in input]
+            results_tuple = [f.result() for f in futures]
+            results = [r[0] for r in results_tuple]
+        
+    if farm is True:
+        collection = MongoClient(db_address[0]).get_database(db_address[1]).get_collection(db_address[2])
+        layers = []
+        strip = []
+        for count, r in enumerate(results):
+            if type(r) is dict:
+                layers.append(r)
+                strip.append(count)
+
+        if len(layers) != 0:
+            data = {
+                "outline": outline,
+                "layers": layers,
+                "env": {},
+            }
+
+            cue_api, cue_version = cue_address
+            result = request(cue_api, cue_version, "create_job", data)["data"][0]
+
+            for s in strip:
+                results[s] = result
+
+                collection.update_one({"_id": ObjectId(results_tuple[s][1])},{
+                    "$set":{
+                        "thumbnail": result
+                    }
+                })        
+    if single:
+        results = results[0]
+
+    return results
+                
+# Add process to api
+app = FastAPI()
+@app.post("/process")
+async def run(request: Request):
+    request = await request.json()
+    #result = process(**request)
+    result = parallel(**request)
+    return result
+
+# Start api
+if __name__ == "__main__":
+    uvicorn.run(
+        f"{__name__}:app",
+        host="0.0.0.0",
+        port=8000,
+        log_level="info",
+        workers=1,
+        reload = True
+    )
+
+    
---
+++ src/frame_analysis.py
@@ -0,0 +1,315 @@
+from process import thread, retrieve
+import os
+from pathlib import PurePath
+import glob
+import gc
+import json
+from sys import argv
+from bson.objectid import ObjectId
+os.environ["OPENCV_IO_ENABLE_OPENEXR"] = "1"
+try:
+    import cv2
+    import clique
+    import ffmpegcv
+    import PyOpenColorIO as OCIO
+    from pymongo import MongoClient
+except ImportError:
+    import pip, site
+    from importlib import reload
+
+    pip.main(["install", "opencv-contrib-python", "-q"])
+    pip.main(["install", "clique", "-q"])
+    pip.main(["install", "ffmpegcv", "-q"])
+    pip.main(["install", "opencolorio", "-q"])
+    pip.main(["install", "pymongo", "-q"])
+    reload(site)
+    import cv2
+    import clique
+    import ffmpegcv
+    import PyOpenColorIO as OCIO
+    from pymongo import MongoClient
+import numpy as np
+
+
+def get_quality(frame_cap, quality, num_frames):
+
+    # Set no frame_cap
+    if frame_cap <= 0:
+        frame_cap = num_frames
+
+    # Create frame incrememnt value
+    target_quality = round(num_frames * quality)
+    increment = num_frames / min(frame_cap, target_quality)
+
+    # Correct broken values
+    if increment > num_frames:
+        increment = num_frames
+
+    # Create frame numbers to process
+    step = 0
+    a_frames = [0]
+    while step < num_frames - 1:
+        step += increment
+        rounded = round(step)
+        if rounded <= num_frames - 1:
+            a_frames.append(rounded)
+
+    # Create single quaity value for database
+    store_quality = len(a_frames) / num_frames
+    return a_frames, store_quality, num_frames
+
+
+def get_paths(input, store_quality):
+
+    path = PurePath(input)
+
+    fps = False
+    exr = False
+    a_frames, store_quality, num_frames = store_quality
+
+    if len(path.suffixes) == 1:
+        vid = ffmpegcv.noblock(ffmpegcv.VideoCapture, input)
+        if num_frames is None:
+            num_frames = vid.count
+            a_frames, store_quality, num_frames = get_quality(
+                a_frames, store_quality, num_frames
+            )
+        fps = round(vid.fps)
+
+        run = True
+        files = []
+        while run:
+            run, img = vid.read()
+            if not run:
+                break
+            files.append(img.copy())
+
+        vid.release()
+        frame_list = list(range(1, num_frames + 1))
+    else:
+        pattern = path.name.split(".")[0] + "*" + path.suffix
+        files = glob.glob(str(PurePath(path.parent / pattern)))
+        assembly = clique.assemble(files)[0][0]
+        frame_list = assembly.indexes
+        if assembly.tail == ".exr":
+            exr = True
+        if num_frames is None:
+            num_frames = len(frame_list)
+            a_frames, store_quality, num_frames = get_quality(
+                a_frames, store_quality, num_frames
+            )
+
+    frame_paths = []
+    for f, frame in enumerate(frame_list):
+
+        # Frames to ignore
+        if len(a_frames) == 0:
+            check = False
+        elif f != a_frames[0]:
+            check = False
+
+        else:
+            # Pixel arrays from video
+            if fps:
+                check = files[frame - 1]
+
+            # File paths from sequence
+            else:
+                check = (
+                    assembly.head + str(frame).zfill(assembly.padding) + assembly.tail
+                )
+            a_frames.pop(0)
+
+        frame_paths.append([check, fps, exr])
+
+    return frame_paths, num_frames, fps, store_quality
+
+
+def aces_srgb(read):
+
+    # Get available colour spaces
+    config = OCIO.Config.CreateFromEnv()
+    if not config.getName():
+        config = OCIO.Config.CreateFromBuiltinConfig(
+            "ocio://cg-config-v2.1.0_aces-v1.3_ocio-v2.3"
+        )
+
+    aces = None
+    srgb = None
+    cs = config.getColorSpaceNames()
+    for color in cs:
+        if "ACEScg" in color and not aces:
+            aces = color
+        elif "Rec.709" in color and not srgb:
+            srgb = color
+        if aces and srgb:
+            break
+
+    if not aces or not srgb:
+        raise ValueError(
+            "Could not find the appropriate color spaces in the OCIO configuration"
+        )
+
+    if read.dtype != np.float32:
+        read = read.astype(np.float32)
+
+    # Remove alpha
+    if read.shape[2] == 4:
+        read = read[:, :, :3]
+
+    rgb = cv2.cvtColor(read, cv2.COLOR_BGR2RGB)
+
+    # Convert to rec 709
+    processor_2 = config.getProcessor(aces, srgb)
+    cpu_2 = processor_2.getDefaultCPUProcessor()
+    cpu_2.applyRGB(rgb)
+
+    # Adjust Gamma
+    transform = OCIO.ExposureContrastTransform()
+    transform.setStyle(OCIO.ExposureContrastStyle(2))
+    transform.setExposure(1)
+
+    # Apply Changes
+    processor_1 = config.getProcessor(transform)
+    cpu_1 = processor_1.getDefaultCPUProcessor()
+    cpu_1.applyRGB(rgb)
+    converted_rgb = np.clip(rgb, 0, 1)
+
+    converted_rgb_8bit = cv2.cvtColor(
+        (converted_rgb * 255).astype(np.uint8), cv2.COLOR_RGB2BGR
+    )
+
+    return converted_rgb_8bit
+
+
+def fit(array, new_max):
+
+    # normalise array values between 0 and 1
+    old_min = min(array)
+    old_max = max(array)
+    n_array = []
+    for a in array:
+        if old_max - old_min == 0:
+            result = 0
+        else:
+            result = ((a - old_min) * new_max) / (old_max - old_min)
+        n_array.append(result)
+    return n_array
+
+
+def color_var(path, fps, exr):
+
+    if path is False:
+        return 0.0
+    elif fps:
+        image = path
+    elif exr:
+        image = cv2.imread(path, -1)
+        image = aces_srgb(image)
+    else:
+        image = cv2.imread(path)
+
+    # Convert to HSV
+    HSV = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    H, S, V = cv2.split(HSV.astype("float"))
+
+    # Standard deviation of the hue
+    h_std = np.std(H)
+    # 50th percentile of the saturation
+    s_ndp = np.mean(S) + (0.5 * np.std(S))
+    # 50th percentile of the value
+    v_ndp = np.mean(V) + (0.5 * np.std(V))
+
+    out = s_ndp * v_ndp * h_std
+    # Correct for invalid values
+    if out == 0.0:
+        out = 1.0
+
+    return out
+
+
+def framing(path, fps, exr):
+
+    if path is False:
+        return 0.0
+    elif fps:
+        image = path
+    elif exr:
+        image = cv2.imread(path, -1)
+        image = aces_srgb(image)
+    else:
+        image = cv2.imread(path)
+
+    # Detect edges and make contours
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    blurred = cv2.GaussianBlur(gray, (3, 3), 0)
+    edges = cv2.Canny(blurred, 0, 175)
+    contours = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[0]
+
+    # Create a rotated bounding box and calc area
+    if len(contours) == 0:
+        area = 1.0
+    else:
+        rect = cv2.minAreaRect(contours[0])
+        area = rect[1][0] * rect[1][1]
+
+    return area
+
+
+def main(input, store_quality, color_weight, focus_weight, db_address, id):
+
+    gc.collect()
+
+    frame_paths, num_frames, fps, store_quality = get_paths(input, store_quality)
+
+    # calculate colour variation and framing
+    cv_threads = thread(color_var, frame_paths)
+    f_threads = thread(framing, frame_paths)
+    color_results, framing_results = retrieve(cv_threads, f_threads)
+
+    # Implement custom score ratio
+    if color_weight == -1:
+        normalised_color = color_results
+    else:
+        normalised_color = fit(color_results, color_weight)
+
+    if focus_weight == -1:
+        normalised_framing = framing_results
+    else:
+        normalised_framing = fit(framing_results, focus_weight)
+
+    # Get largest score
+    score = [normalised_color[i] + normalised_framing[i] for i in range(num_frames)]
+    index = score.index(max(score))
+
+    # Get highest scoring frame
+    if fps:
+        output = round(index / fps, 2)
+    else:
+        output = (list(frame_paths)[index])[0]
+
+    # Get Mongo DB
+
+    collection = (
+        MongoClient(db_address[0])
+        .get_database(db_address[1])
+        .get_collection(db_address[2])
+    )
+
+    collection.update_one(
+        {"_id": ObjectId(id)},
+        {
+            "$set": {
+                "thumbnail": output,
+                "quality": store_quality,
+                "num_frames": num_frames,
+                "processing": False,
+            }
+        },
+    )
+
+    return output
+
+
+if __name__ == "__main__":
+    main(**json.loads(argv[1]))
